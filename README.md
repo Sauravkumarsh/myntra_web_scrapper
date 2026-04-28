a08baa06fec699bc525ba8663d18c243535cba52
Sauravsh <kumarsaurav2905@gmail.com>
2026-04-28 12:21:40 +0530

    This is my first Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Myntra_review_Scrapper Project
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='saurav',
+    author_email='kumsaurav2905@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
